94cc00d44e653c6604ac62425990ea1150dbe72d
yiqu <291682648@qq.com>
2017-02-17 23:49:58 +0800

    version .2

--- src/Input.java
+++ src/Input.java
@@ -1,29 +1,19 @@
-
 import java.io.*;
 public class Input {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		String path="C:\\Users\\Hacker\\Desktop\\a.txt";
-		
-	
-		   try {
-			PrintWriter out=new PrintWriter(new OutputStreamWriter(new FileOutputStream("c:/utf-8.txt"),"UTF-8"));
-		
-			out.write("汗");
-			out.println("dfsdf");
-			out.close();
-		   
-		   } catch (UnsupportedEncodingException e) {
+		try(Reader in=new FileReader("C:\\Users\\Hacker\\Desktop\\qq.txt"))
+		{
+			char[] bbuf=new char[1024];
+			while(in.read(bbuf)>0)
+			{
+				System.out.println("");
+				System.out.println(new String(bbuf));
+			}
+		} catch ( IOException e){
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		} 
-		
-		   
-		
-	
+		}
 	}
 }
