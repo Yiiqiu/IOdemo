aaaa02f2b70fea412a24fe5cce770232aae6fc20
yiqu <291682648@qq.com>
2017-02-18 22:55:28 +0800

    version 3.0

--- src/Input.java
+++ src/Input.java
@@ -1,17 +1,22 @@
 import java.io.*;
+import java.nio.charset.Charset;
 public class Input {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		try(Reader in=new FileReader("C:\\Users\\Hacker\\Desktop\\qq.txt"))
+		try	
 		{
-			char[] bbuf=new char[1024];
-			while(in.read(bbuf)>0)
-			{
-				System.out.println("");
-				System.out.println(new String(bbuf));
-			}
-		} catch ( IOException e){
+			//Reader in=new FileReader("C:\\Users\\Hacker\\Desktop\\c.txt");
+            BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Hacker\\Desktop\\c.txt"),"UTF-8"));
+            int a;
+            String line;  
+            StringBuffer stringBuffer = new StringBuffer();  
+            while((line=br.readLine())!=null)
+            {
+            	stringBuffer.append(line);
+            }
+            System.out.println(stringBuffer);
+		} catch ( IOException e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
